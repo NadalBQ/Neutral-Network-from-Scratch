28e80e12909bec33f146182d9aea3a335805cce7
NadalBQ <nadalbq@gmail.com>
2026-02-25 12:02:54 +0100

    Add Softmax activation layer with forward and backward passes

--- nn/activation.py
+++ nn/activation.py
@@ -37,6 +37,7 @@ class Sigmoid:
     def backward(self, dA):
         return dA * (self.A_cache * (1.0 - self.A_cache))
     
+
 class Tanh:
     def forward(self, Z) -> np.ndarray:
         self.A_cache = np.tanh(Z)   
@@ -46,6 +47,27 @@ class Tanh:
         return dA * (1.0 - self.A_cache * self.A_cache)
 
 
+class Softmax:
+    def forward(self, Z) -> np.ndarray:
+        Z_stable = Z - np.max(Z, axis=1, keepdims=True)
+        expZ = np.exp(Z_stable)
+        self.A_cache = expZ / np.sum(expZ, axis=1, keepdims=True)
+        return self.A_cache
+
+    def backward(self, dA):
+        """
+        dA: gradiente de la loss respecto a la salida softmax (batch_size, n_classes)
+        Usamos la fórmula simplificada para cross-entropy + softmax:
+        dZ = A - Y  si loss = categorical cross-entropy
+        Para grad general: dZ_i = sum_j dA_j * ∂A_j/∂Z_i
+        """
+        batch_size, n_classes = dA.shape
+        dZ = np.empty_like(dA)
+        for i in range(batch_size):
+            a = self.A_cache[i].reshape(-1,1)
+            jacobian = np.diagflat(a) - a @ a.T
+            dZ[i] = jacobian @ dA[i]
+        return dZ
 
 
 
